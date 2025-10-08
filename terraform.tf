05095abff59474055e0304ea55e55967c3c06240
Satoshi I <8ato2h1@gmail.com>
2025-10-08 17:01:05 +0900

    versioning

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  /* Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
-    organization = "organization-name"
+    organization = "satoshi-itaki-org"
     workspaces {
       name = "learn-terraform-provider-versioning"
     }
   }
-  */
+
 
   required_providers {
     random = {
